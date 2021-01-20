949ee66e479acfeca1cc1f4ceaddd0b37a0121c9
YW <qingyi_w@outlook.com>
2021-01-20 17:35:54 +0800

    optimize code

--- index/index.js
+++ index/index.js
@@ -2,46 +2,63 @@ const app = getApp()
 
 Page({
   data: {
-    // code: ''
     items: [
       {
         src: '/imgs/1.jpg',
-        title: '标题',
-        artist: '简介',
+        title: '动漫头像',
+        artist: '动漫女生头像，简笔画头像风格，唯美简约风格，可爱甜美风格',
         price: 20,
         number: 0,
         selectedNum: 0
       },
       {
         src: '/imgs/2.jpg',
-        title: '标题',
-        artist: '简介',
-        price: 25,
+        title: '动漫女生头像',
+        artist: '动漫女生头像，简笔画头像风格，唯美简约风格，可爱甜美风格，二次元',
+        price: 30,
         number: 0,
         selectedNum: 0
       }
-    ]
+    ],
+    total: 0
   },
-  // login() {
-  //   console.log('login')
-  //   let self = this
-  //   wx.login({
-  //     success(res) {
-  //       console.log(res)
-  //       self.setData({
-  //         code: res.code
-  //       })
-  //     }
-  //   })
-  // },
+  minusNum: function (event) {
+    const index = event.currentTarget.dataset.index
+    let items = this.data.items
+    items[index].selectedNum--
+    this.setData({
+      items: items
+    })
+    this.getTotal()
+  },
+  plusNum: function (event) {
+    const index = event.currentTarget.dataset.index
+    let items = this.data.items
+    items[index].selectedNum++
+    this.setData({
+      items: items
+    })
+    this.getTotal()
+  },
+  getTotal: function () {
+    let totalNum = 0
+    for (const item of this.data.items) {
+      totalNum += item.selectedNum * item.price
+    }
+    this.setData({
+      total: totalNum
+    })
+  },
+
   onLoad() {
     let self = this
     wx.login({
       success(res) {
         console.log(res)
         if (res.code) {
+          console.log(res.code)
           // wx.request({
-          //   url: 'http://127.0.0.1/8080',
+          //   url: 'http://127.0.0.1:3000',
           //   data: {
           //     code: res.code
           //   }
@@ -49,10 +66,6 @@ Page({
         } else {
           console.log('login error')
         }
-        
-        // self.setData({
-        //   code: res.code
-        // })
       }
     })
   },
