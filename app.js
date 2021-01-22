fe817f05005ea975571ec347de652372ffabf344
YW <qingyi_w@outlook.com>
2021-01-22 17:49:38 +0800

    add shop

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 App({
   globalData: {
+    storeId: 0,
+    userId: 0,
     serverUrl: 'http://127.0.0.1:3000'
+    // serverUrl: 'http://192.168.199.198:5000'
   },
   onLaunch() {
   }
--- index/index.js
+++ index/index.js
@@ -2,6 +2,10 @@ const app = getApp()
 
 Page({
   data: {
+    userInfo: {
+      avatarUrl: '/imgs/account_707070_81.png',
+      nickName: '未登录用户'
+    },
     items: [
       {
         src: '/imgs/1.jpg',
@@ -22,19 +26,60 @@ Page({
     ],
     total: 0
   },
+  redirectToStorePage: function (event) {
+    wx.navigateTo({
+      url: '/shop/shop',
+    })
+  },
+  getUserInfo: function (event) {
+    console.log(event)
+    const iv = event.detail.iv
+    const encryptedData = event.detail.encryptedData
+    // const avatar = event.detail.
+    // let self = this
+    const userInfo = event.detail.userInfo
+    this.setData({
+      userInfo: userInfo
+    })
+    wx.login({
+      success(res) {
+        if (res.code) {
+          wx.request({
+            url: app.globalData.serverUrl + '/api/v1/wx/users/oauth_user',
+            data: {
+              code: res.code,
+              encrypted_data: encryptedData,
+              iv: iv
+            },
+            method: 'POST',
+            success (res) {
+              if (res.data.data.user_id) {
+                console.log('userid: ' + res.data.data.user_id)
+              }
+            },
+            fail (err) {
+              console.error(err)
+            }
+          })
+        } else {
+          console.log('login error')
+        }
+      }
+    })
+  },
   pay: function (event) {
     const total = this.data.total
     console.log(total)
     wx.showToast({
       title: '支付成功！',
       icon: 'success',
-      duration: 20000
+      duration: 2000
     })
     setTimeout(() => {
       wx.switchTab({
         url: '/info/info',
       })
-    }, 2000)
+    }, 2010)
   },
   minusNum: function (event) {
     const index = event.currentTarget.dataset.index
@@ -65,22 +110,16 @@ Page({
   },
 
   onLoad() {
-    let self = this
-    wx.login({
-      success(res) {
-        console.log(res)
-        if (res.code) {
-          console.log(res.code)
-          // wx.request({
-          //   url: 'http://127.0.0.1:3000',
-          //   data: {
-          //     code: res.code
-          //   }
-          // })
-        } else {
-          console.log('login error')
-        }
-      }
-    })
+    // wx.request({
+    //   method: 'GET',
+    //   url: app.globalData.serverUrl + '',
+    //   data: {},
+    //   success (res) {
+
+    //   },
+    //   fail (err) {
+
+    //   }
+    // })
   },
 })
