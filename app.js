5a075bfd0e11711de6a99ad93d2cd66297c1326a
YW <qingyi_w@outlook.com>
2021-01-27 11:53:05 +0800

    change server url

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ App({
     shopInfo: {},
     userInfo: {},
     // serverUrl: 'http://127.0.0.1:3000'
-    serverUrl: 'http://192.168.199.198:5000'
+    // serverUrl: 'http://192.168.199.198:5000'
+    serverUrl: 'https://beer-dev.sg-ai.com'
   },
   onLaunch() {
   }
